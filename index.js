753e05489fb124dfe6d55554f7be66b5b7214a22
Storm <lenagreidon@mail.ru>
2022-11-29 20:48:41 +0300

    done

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require("express");
 const cors = require("cors");
-const router=require('./routes/index');
+const router=require('./routes/indexRout');
 
 const PORT = process.env.PORT || 5000;
 
